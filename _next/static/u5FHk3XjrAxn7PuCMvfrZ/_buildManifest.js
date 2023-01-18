fe7163cbdd241245c31a8e4610152803557e4b5e
Saad Mir <saad@softwaremakers.com>
2023-01-17 16:47:32 -0800

    second

---
+++ _next/static/u5FHk3XjrAxn7PuCMvfrZ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/d078c8622f21e20b.css","static/chunks/pages/index-2685f09c07d46733.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
